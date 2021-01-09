b148c170d0017ad6be692d7a6fb89891a2d023b8
Абакар <akap302@gmail.com>
2021-01-09 03:23:24 +0300

    add bcg

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const input = document.querySelector('.input');
 const span = document.querySelector('span');
+const btn = document.querySelector('.btn')
+const body = document.querySelector('body');
 
 let arr = input.value;
 
 input.addEventListener('input', function () {
     span.textContent = input.value.length;
+})
+
+
+btn.addEventListener('click', () => {
+    let red = Math.random() * 255;
+    let green = Math.random() * 255;
+    let blue = Math.random() * 255;
+    console.log(red)
+    body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
 })
 
